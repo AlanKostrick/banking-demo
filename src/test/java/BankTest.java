301f3f408a36654befa373ec97e269b682f60616
AlanKostrick <alan@wecancodeit.org>
2019-05-22 11:33:23 -0400

    test collections maintance class

---
+++ src/test/java/BankTest.java
@@ -0,0 +1,73 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.Test;
+
+public class BankTest {
+	
+	Account account1 = new Account(500, "Uncle Bob");
+	Account account2 = new Account(400, "Marty Fowler");
+	Bank underTest = new Bank();
+	
+	@Test
+	public void shouldAddAnAccountToTheBank() {
+		underTest.addAccount(account1);
+		int numAccounts = underTest.getNumAccounts();
+		assertThat(numAccounts, is(1));
+	}
+	
+	@Test
+	public void shouldAddTwoAccountsToTheBank() {
+		underTest.addAccount(account1);
+		underTest.addAccount(account2);
+		int numAccounts = underTest.getNumAccounts();
+		assertThat(numAccounts, is(2));
+		assertThat(numAccounts,is(notNullValue()));
+	}
+	
+	@Test
+	public void shouldReturnBalanceOf500WhenFindingAccount1() {
+		underTest.addAccount(account1);
+		Account acctToFind = underTest.findAccount("Uncle Bob");
+		int foundBalance = acctToFind.getBalance();
+		assertThat(foundBalance, is(500));
+	}
+	
+	@Test
+	public void shouldCloseAnAccountAtTheBank() {
+		underTest.addAccount(account1);
+		underTest.addAccount(account2);
+		underTest.closeAccount(account1);
+		int numAccounts = underTest.getNumAccounts();
+		assertThat(numAccounts, is(1));
+	}
+	
+	@Test
+	public void shouldHaveBalancesOf600And500AfterDepositAllOf100() {
+		underTest.addAccount(account1);
+		underTest.addAccount(account2);
+		underTest.depositAll(100);
+		assertThat(account1.getBalance(), is(600));
+		assertThat(account2.getBalance(), is(500));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
